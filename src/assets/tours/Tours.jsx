76f7ef1ba3ded0953152ebcf1432cf7558776c6a
Yko-koshka <legezaanna7@gmail.com>
2024-07-19 17:23:12 +0200

    added tours folder

---
+++ src/assets/tours/Tours.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+const url = 'https://www.course-api.com/react-tours-project'
+
+const Tours = () => {
+  const [isLoading, setIsLoading] = useState()
+  const [tours, setTours] = useState()
+
+  return <></>
+}
+export default Tours
