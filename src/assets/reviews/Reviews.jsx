6d9a21571f2b5cbabaa10dd65710b806b62796b5
Yko-koshka <legezaanna7@gmail.com>
2024-08-28 17:54:21 +0200

    output data on the page

--- src/assets/reviews/Reviews.jsx
+++ src/assets/reviews/Reviews.jsx
@@ -11,7 +11,13 @@ const Reviews = () => {
       <article className="review">
         <div className="img-container">
           <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
         </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
       </article>
     </main>
   )
