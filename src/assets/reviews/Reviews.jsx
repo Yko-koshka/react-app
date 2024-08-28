15e5afcab333c00c178665b4248528c9ba83cb44
Yko-koshka <legezaanna7@gmail.com>
2024-08-28 17:46:31 +0200

    added a library react-icons

--- src/assets/reviews/Reviews.jsx
+++ src/assets/reviews/Reviews.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
 const Reviews = () => {
   const [index, setIndex] = useState(0)
   const { name, job, image, text } = people[index]
 
-  return <main></main>
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+        </div>
+      </article>
+    </main>
+  )
 }
 
 export default Reviews
